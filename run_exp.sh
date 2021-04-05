21052abcedc5f93df5f3d5eda31ac3fcdb42eeb8
skywalker666666 <zhiliu_yang@hotmail.com>
2021-04-05 14:52:08 -0400

    add raw script

--- run_exp.sh
+++ run_exp.sh
@@ -23,5 +23,10 @@
 
 
 
-python upsnet/core_upsnet_end2end_test_multiple_imgs.py --cfg upsnet/experiments/upsnet_resnet50_coco_4gpu.yaml --weight_path /home/zhiliu/Documents/Panoptic_Segement/Cocopanopticapi/UnifiedPanopticSeg/UPSNet_PanMapping/model/upsnet_resnet_50_coco_90000
+#python upsnet/core_upsnet_end2end_test_multiple_imgs.py --cfg upsnet/experiments/upsnet_resnet50_coco_4gpu.yaml --weight_path /home/zhiliu/Documents/Panoptic_Segement/Cocopanopticapi/UnifiedPanopticSeg/UPSNet_PanMapping/model/upsnet_resnet_50_coco_90000
 
+
+# trainning script 03282021
+#python upsnet/upsnet_end2end_train.py --cfg upsnet/experiments/upsnet_resnet50_scannet_1gpu.yaml 
+
+python upsnet/upsnet_end2end_test.py --cfg upsnet/experiments/upsnet_resnet50_scannet_1gpu.yaml --weight_path output/upsnet/scannet/upsnet_resnet50_scannet_1gpu/scannettrain2021/upsnet_resnet_50_scannet_126000.pth
